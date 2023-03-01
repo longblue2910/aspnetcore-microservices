34045471a1c4404eacfd3d95c577478c820790f9
longblue2910 <long.pt@isdcorp.vn>
2023-03-01 14:31:50 +0700

    Update code

--- src/BuildingBlocks/Infrastructure/Services/SmtpEmailService.cs
+++ src/BuildingBlocks/Infrastructure/Services/SmtpEmailService.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Contracts.Configurations;
+using Contracts.Services;
+using Serilog;
+using Shared.Services.Email;
 
 namespace Infrastructure.Services
 {
-    internal class SmtpEmailService
+    public class SmtpEmailService : IEmailService<MailRequest>
     {
+        private readonly ILogger _logger;
+        private readonly IEmailSMTPSettings _setting;
+
+        public SmtpEmailService(ILogger logger, IEmailSMTPSettings setting)
+        {
+            _logger = logger;
+            _setting = setting;
+        }
+
+        public void SendEmailAsync(MailRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
